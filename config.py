7eeb1b8e5cde0947d6ad29f42002d151b2dd0938
toEpam <toepammiddle@gmail.com>
2024-01-21 04:12:35 +0500

    .

---
+++ config.py
@@ -0,0 +1,37 @@
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = "6821103467:AAE9EDp9jEcwGF1PGobv7S74TEefhxUwrio"
+    OWNER_ID = "557407324" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "ToEpamMiddle"
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'postgres://hasan19:manager19@165.232.72.127:5432/gymdb'  # needed for any database modules
+    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    LOAD = []
+    NO_LOAD = ['translation', 'rss']
+    WEBHOOK = False
+    URL = None
+
+    # OPTIONAL
+    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    STRICT_GBAN = False
+    STRICT_GMUTE = False
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    ALLOW_EXCL = False  # Allow ! commands as well as /
+
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
